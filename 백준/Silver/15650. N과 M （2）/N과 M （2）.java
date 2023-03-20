7ef6169f71ace0b00d1462d4a7da00d674d649cd
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-03-21 02:44:54 +0900

    [Silver III] Title: N과 M (2), Time: 144 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int n, m;
+	static int[] arr;
+	static boolean[] visited;
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	n = Integer.parseInt(st.nextToken());
+    	m = Integer.parseInt(st.nextToken());
+    	
+    	arr = new int[m];
+    	visited = new boolean[n];
+    	
+    	dfs(0, 0);
+    	System.out.println(sb);
+    } 
+    public static void dfs(int start, int depth) {
+    	if(depth == m) {
+    		for(int i : arr) {
+    			sb.append(i+" ");
+    		}
+    		sb.append("\n");
+    		return;
+    	}
+    	for(int i = start; i < n; i++) {
+    		if(!visited[i]) {
+    			visited[i] = true;
+    			arr[depth] = i+1;
+    			dfs(i + 1, depth+1);
+    			visited[i] = false;
+    		}
+    	}
+    }
+}
