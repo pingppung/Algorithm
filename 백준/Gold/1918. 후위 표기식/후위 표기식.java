41161b118d47032f8ca91f045bb680cf67db3cf3
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-03-24 13:23:46 +0900

    [Gold II] Title: 후위 표기식, Time: 120 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+    	
+    	String s = br.readLine();
+    	Stack<Character> stack = new Stack<>();
+    	
+    	for(int i = 0; i < s.length(); i++) {
+    		char c = s.charAt(i);
+    		switch(c) {
+    			case '+':
+    			case '-':
+    			case '*':
+    			case '/':
+    				while(!stack.isEmpty() && priority(stack.peek()) >= priority(c)) {
+    					sb.append(stack.pop());
+    				}
+    				stack.add(c);
+    				break;
+    			case '(':
+    				stack.add(c);
+    				break;
+    			case ')':
+    				while(!stack.isEmpty() && stack.peek() != '(') {
+    					sb.append(stack.pop());
+    				}
+    				stack.pop();
+    				break;
+    			default:
+    				sb.append(c);
+    					
+    		}
+    	}
+    	while(!stack.isEmpty()) {
+    		sb.append(stack.pop());
+    	}
+    	System.out.println(sb);
+    	
+    } 
+    public static int priority(char c) {
+		if(c == '(' || c == ')') return 0;
+		else if(c == '+' || c == '-') return 1;
+		else if(c == '*' || c == '/') return 2;
+		return -1;
+	}
+}
