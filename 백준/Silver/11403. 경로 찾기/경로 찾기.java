158817bdc71590b43f16c1729a7f60eff8d29f8d
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-04-01 02:30:53 +0900

    [Silver I] Title: 경로 찾기, Time: 232 ms, Memory: 18836 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	static int n;
+	static int[][] arr;
+
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	StringBuilder sb = new StringBuilder();
+    	
+    	n = Integer.parseInt(br.readLine());
+    	arr = new int[n][n];
+    	for(int i = 0; i < n; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		for(int j = 0; j < n; j++) {
+    			arr[i][j] = Integer.parseInt(st.nextToken());
+    		}
+    	}
+    	for(int k = 0; k < n; k++) {
+    		for(int i = 0; i < n; i++) {
+        		for(int j = 0; j < n; j++) {
+        			if (arr[i][k] == 1 && arr[k][j] == 1) {
+                        arr[i][j] = 1;
+                    }
+        		}
+        	}
+		}
+    	 for(int i = 0; i < n; i++) {
+             for(int j = 0; j < n; j++) {
+                 sb.append(arr[i][j] + " ");
+             }
+             sb.append("\n");
+         }
+    	 System.out.println(sb);
+    }
+    
+}
