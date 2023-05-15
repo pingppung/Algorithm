c686f8c34e35a391b6a97b2eb44454d74ab99a38
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-05-15 13:03:51 +0900

    [Silver III] Title: 돌 게임 5, Time: 120 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        if(n%2 == 0) System.out.println("CY");
+    	else System.out.println("SK");
+    }
+}
