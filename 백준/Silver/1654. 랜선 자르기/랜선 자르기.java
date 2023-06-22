f31ae8471d3bbedc8609f29317bccd2ef4d9aa4a
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-06-22 17:14:45 +0900

    [Silver II] Title: 랜선 자르기, Time: 196 ms, Memory: 17864 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,36 +1,36 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int[] arr;
-    public static void main(String[] args) throws IOException{
+	static StringBuilder sb = new StringBuilder();
+	static int[] lines;
+	static int n, k, count = 0;
+	public static void main(String[] args) throws IOException{
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
     	
-    	int k = Integer.parseInt(st.nextToken());
-    	int n = Integer.parseInt(st.nextToken());
-    	
-    	arr = new int[k];
-        
-    	long min = 0;
+    	k = Integer.parseInt(st.nextToken());
+    	n = Integer.parseInt(st.nextToken());
+    	lines = new int[k];
     	long max = 0;
-    	
     	for(int i = 0; i < k; i++) {
-    		arr[i] = Integer.parseInt(br.readLine());
-    		max = Math.max(max, arr[i]);
+    		lines[i] = Integer.parseInt(br.readLine());
+    		max = Math.max(lines[i], max);
     	}
-    	max++;
-    	
-    	while(min < max) {
-    		long mid = (min + max)/2;
-    		long count = 0;
-    		for(int i = 0;  i < k; i++) {
-    			count += (arr[i]/mid);
-    		}
-    		if(count < n) max = mid;
-    		else min = mid +1;
-    	}
-    	
-    	System.out.println(min -1);
-    	
+    	long len = cut_line(max);
+    	System.out.println(len);
     }
+	static long cut_line(long max) {
+		long start = 1;
+		long end = max;
+		while(start <= end) {
+			long count = 0;
+			long mid = (start+end)/2;
+			for(int i = 0; i < k; i++) {
+				count += lines[i]/mid;
+			}
+			if(count >= n) start = mid+1;
+			else end = mid-1;
+		}
+		return end;
+	}
 }
