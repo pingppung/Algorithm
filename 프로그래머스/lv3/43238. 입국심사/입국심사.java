246a9b802f22a35af61cbaa9c6d03796d3854de9
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-06-24 00:05:58 +0900

    [level 3] Title: 입국심사, Time: 0.38 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        Arrays.sort(times);
+        long start = 0;
+        long end = (long) n * times[times.length - 1]; 
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long count = 0; 
+            for (int i = 0; i < times.length; i++) { 
+                count += mid / times[i];
+            }
+            if (count < n) { 
+                start = mid + 1;
+            } else { 
+                end = mid - 1;
+                answer = mid;
+            }
+        }
+        return answer;
+    }
+}
