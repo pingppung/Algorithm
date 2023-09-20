3f8bd6bbc56ee34900c6c25afd234962e4b29557
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-09-21 02:30:08 +0900

    [Silver III] Title: N과 M (1), Time: 332 ms, Memory: 33312 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,35 +2,33 @@ import java.io.*;
 import java.util.*;
 public class Main {
 	static int n, m;
-	static int[] arr;
+	static int[] graph;
 	static boolean[] visited;
 	static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
- 	
-    	n = Integer.parseInt(st.nextToken());
-    	m = Integer.parseInt(st.nextToken());
-    	
-    	arr = new int[m];
-    	visited = new boolean[n];
-    	dfs(0, 0);
-    	System.out.println(sb);
-    	
-    }
-    public static void dfs(int start, int depth) {
-    	if(depth == m) {
-    		for(int i : arr) sb.append(i +" ");
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		graph = new int[m];
+		visited = new boolean[n];
+		dfs(0,0);
+		System.out.println(sb);
+	}
+	public static void dfs(int start, int depth) {
+		
+		if(depth == m) {
+			for(int i : graph) sb.append(i +" ");
     		sb.append("\n");
     		return;
-    	}
-    	for(int i = 0; i < n; i++) {
-    		if(!visited[i]) {
-    			visited[i] = true;
-    			arr[depth] = i+1;
-    			dfs(i, depth+1);
-    			visited[i] = false;
-    		}
-    	}
-    }
+		}
+		for(int i = 0; i < n; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				graph[depth] = i+1;
+				dfs(i, depth+1);
+				visited[i] = false;		
+			}
+		}
+	}
 }
