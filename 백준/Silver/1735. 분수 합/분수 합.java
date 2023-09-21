9844f3e3fc3326d615eef3a0eddad97fc9f4980f
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-09-21 15:13:38 +0900

    [Silver III] Title: 분수 합, Time: 152 ms, Memory: 16040 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.java
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -1,33 +1,31 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    
-    	int ja1 = Integer.parseInt(st.nextToken());
-    	int mo1 = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a1 = Integer.parseInt(st.nextToken());
+    	int b1 = Integer.parseInt(st.nextToken());
     	st = new StringTokenizer(br.readLine());
-    	int ja2 = Integer.parseInt(st.nextToken());
-    	int mo2 = Integer.parseInt(st.nextToken());
+    	int a2 = Integer.parseInt(st.nextToken());
+    	int b2 = Integer.parseInt(st.nextToken());
     	
-    	int result_mo = mo1 * mo2;
-    	int result_ja = mo1*ja2 + mo2*ja1;
+    	int result_mo = b1 * b2;
+    	int result_ja = a1*b2 + a2*b1;
     	
     	int mod = gcd(result_ja, result_mo);
     	result_ja /= mod;
     	result_mo /= mod;
     	
     	System.out.println(result_ja + " "+ result_mo);
-    }
-    public static int gcd(int a, int b) {
-    	if(a <= b) {
-    		int tmp = a;
-    		a = b;
-    		b = tmp;
-    	}
-    	if(b == 0) return a;
-    	return gcd(b, a % b);
-    }
-    
+	}
+	public static int gcd(int a, int b) {
+		while(b != 0) {
+			int tmp = a % b;
+			a = b;
+			b = tmp;
+		}
+		return a;
+	}
 }
