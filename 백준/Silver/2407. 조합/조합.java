29dfb843809b88efccf8f026af147f3dc32aa865
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-09-21 15:34:03 +0900

    [Silver III] Title: 조합, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.java
+++ 백준/Silver/2407. 조합/조합.java
@@ -1,21 +1,20 @@
 import java.io.*;
+import java.math.*;
 import java.util.*;
-import java.math.BigInteger;
 public class Main {
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-
-    	int n = Integer.parseInt(st.nextToken());
-    	int m = Integer.parseInt(st.nextToken());
-    	
-    
-    	BigInteger ja = BigInteger.ONE;	
-    	BigInteger mo = BigInteger.ONE;
-    	for(int i = 0; i < m; i++) {
-    		ja = ja.multiply(new BigInteger(String.valueOf(n-i)));
-    		mo = mo.multiply(new BigInteger(String.valueOf(i+1)));
-    	}
-    	System.out.println(ja.divide(mo));
-    } 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		BigInteger a = BigInteger.ONE;	
+    	BigInteger b = BigInteger.ONE;
+		for(int i = 0; i < m; i++) {
+			a = a.multiply(new BigInteger(String.valueOf(n-i)));
+			b = b.multiply(new BigInteger(String.valueOf(i+1)));
+		}
+		System.out.println(a.divide(b));
+	}
 }
