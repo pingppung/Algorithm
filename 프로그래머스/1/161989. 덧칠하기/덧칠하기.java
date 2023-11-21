eb3aed497507caa58734e11136086b117c1cc81c
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-21 15:55:26 +0900

    [level 1] Title: 덧칠하기, Time: 1.60 ms, Memory: 94.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int start = section[0];        
+        answer++;                
+        for (int sec : section) {            
+            if (start + m > sec) {                
+                continue;            
+            }                        
+            start = sec;            
+            answer++;        
+        }                
+        return answer;
+
+    }
+}
