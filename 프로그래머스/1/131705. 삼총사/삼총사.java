4df97822f428ae2da0e965e59a214062ad21852f
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-23 14:40:39 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        for(int i = 0; i < number.length-2; i++){
+            for(int j = i+1; j < number.length-1; j++){
+                for(int k = j+1; k < number.length; k++){
+                    int total = number[i] + number[j] + number[k];
+                    if(total == 0) answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
