8375f5ebcdc4550e97d63a643999cb05df4466ea
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-24 12:06:28 +0900

    [level 1] Title: 예산, Time: 0.36 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for(int depart : d){
+            budget -= depart;
+            if(budget < 0) break;
+            answer++;
+        }
+        return answer;
+    }
+}
