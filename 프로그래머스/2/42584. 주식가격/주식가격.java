664d328eb603efd397291ca515b91f68892d9ec7
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-01 13:31:26 +0900

    [level 2] Title: 주식가격, Time: 59.22 ms, Memory: 67.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < prices.length; i++) {
+            st.add(0);
+    		for(int j = i+1; j < prices.length; j++) {
+                st.set(i, st.elementAt(i)+1);
+    			if(prices[i]> prices[j]) break;
+    		}
+    	}
+        return st.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
