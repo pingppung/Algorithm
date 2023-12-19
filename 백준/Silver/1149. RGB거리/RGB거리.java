969902f20a786f7fba28c116cb6f9efd0c1c72d6
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-20 01:40:26 +0900

    [Silver I] Title: RGB거리, Time: 128 ms, Memory: 14564 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	
+    	int n = Integer.parseInt(br.readLine());
+    	int[][] arr = new int[n][3];
+    	
+    	for(int i = 0; i < n; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		arr[i][0] = Integer.parseInt(st.nextToken());  //R
+    		arr[i][1] = Integer.parseInt(st.nextToken());  //G
+    		arr[i][2] = Integer.parseInt(st.nextToken());   //B
+    		
+    	}
+    	for(int i =1; i <n; i++) {
+    		arr[i][0] += Math.min(arr[i-1][1], arr[i-1][2]);
+    		arr[i][1] += Math.min(arr[i-1][0], arr[i-1][2]);
+    		arr[i][2] += Math.min(arr[i-1][0], arr[i-1][1]);
+    	}
+    	System.out.println(Math.min(Math.min(arr[n-1][0],arr[n-1][1]), arr[n-1][2]));
+    	
+    	
+    }
+}  
