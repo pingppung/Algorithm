c9b24b430fa920c83a96cdd7d7736b409efa94cb
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-23 01:54:04 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 45564 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -3,16 +3,15 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int n = Integer.parseInt(br.readLine());
 		Queue<Integer> q = new LinkedList<>();
 		for(int i = 1; i <= n; i++) {
-			q.offer(i);
+			q.add(i);
 		}
-		while(q.size() > 1) {
-			q.poll();
-			q.offer(q.poll());
+		while(q.size() != 1) {
+			q.remove();
+			q.add(q.poll());
 		}
-		System.out.println(q.poll());
+		System.out.println(q.peek());
 	}
 }
