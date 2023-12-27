532b72647b4c0a3f96f5724ba5a20ddafb856ec9
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-28 02:20:59 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 356 ms, Memory: 25840 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,25 +1,22 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    	int n = Integer.parseInt(br.readLine());
-    	PriorityQueue<Long> pq = new PriorityQueue<Long>();
-    	for(int i = 0 ; i < n; i++) {
-    		pq.add(Long.parseLong(br.readLine()));
-    	}
-    	
-    	int result = 0;
-		while (pq.size() > 1) {
-			long num1 = pq.poll();
-			long num2 = pq.poll();
-			result += num1 + num2;
-			pq.add(num1 + num2); 
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Long> cards = new PriorityQueue<>();
+		for(int i = 0; i < n; i++) {
+			cards.add(Long.parseLong(br.readLine()));
 		}
-    	System.out.println(result);
-    	
-    	
-    }
-	
+		
+		int count = 0;
+		while(cards.size() > 1) {
+			long a = cards.poll();
+			long b = cards.poll();
+			count += a + b;
+			cards.add(a + b);
+		}
+		System.out.println(count);
+	}
 }
