2665999eb8751fd1f2954732324e10ab1def18c5
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-28 14:40:28 +0900

    [Silver IV] Title: 대칭 차집합, Time: 744 ms, Memory: 83692 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -1,36 +1,32 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        Set<Integer> setA = new HashSet<>();
-        Set<Integer> setB = new HashSet<>();
-
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < a; i++) {
-            setA.add(Integer.parseInt(st.nextToken()));
-        }
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < b; i++) {
-            setB.add(Integer.parseInt(st.nextToken()));
-        }
-
-        int ans = 0;
-        for(int num : setA) {
-            if(!setB.contains(num)) {
-                ans += 1;
-            }
-        }
-        for(int num : setB) {
-            if(!setA.contains(num)) {
-                ans += 1;
-            }
-        }
-        System.out.println(ans);
-    }
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		HashSet<Integer> a = new HashSet<>();
+		HashSet<Integer> b = new HashSet<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			a.add(Integer.parseInt(st.nextToken()));
+		}
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < m; i++) {
+			b.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		int count = 0;
+		for(int num : a) {
+			if(!b.contains(num)) count++;
+		}
+		for(int num : b) {
+			if(!a.contains(num)) count++;
+		}
+		System.out.println(count);
+	}
 }
