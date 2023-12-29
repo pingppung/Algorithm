e7201128f6399651895e5f692cbfa52eecc885c7
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-30 01:29:57 +0900

    [Silver II] Title: 랜선 자르기, Time: 188 ms, Memory: 17596 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,35 +1,32 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	static int[] lines;
-	static int n, k, count = 0;
-	public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
-    	k = Integer.parseInt(st.nextToken());
-    	n = Integer.parseInt(st.nextToken());
-    	lines = new int[k];
-    	long max = 0;
+	static int[] lanterns;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int k = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		lanterns = new int[k];
+		long max = 0;
     	for(int i = 0; i < k; i++) {
-    		lines[i] = Integer.parseInt(br.readLine());
-    		max = Math.max(lines[i], max);
+    		lanterns[i] = Integer.parseInt(br.readLine());
+    		max = Math.max(lanterns[i], max);
     	}
-    	long len = cut_line(max);
-    	System.out.println(len);
-    }
-	static long cut_line(long max) {
+		System.out.println(cutter(n, max));
+	}
+		
+	private static long cutter(int n, long max) {
 		long start = 1;
 		long end = max;
 		while(start <= end) {
 			long count = 0;
-			long mid = (start+end)/2;
-			for(int i = 0; i < k; i++) {
-				count += lines[i]/mid;
+			long mid = (start + end) / 2;
+			for(int lantern : lanterns) {
+				count += (lantern / mid);
 			}
-			if(count >= n) start = mid+1;
-			else end = mid-1;
+			if(count >= n) start = mid + 1;
+			else end = mid - 1;
 		}
 		return end;
 	}
