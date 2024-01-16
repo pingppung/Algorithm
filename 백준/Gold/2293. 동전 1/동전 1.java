4b85562143af792da4b3087fe68a84fd997a6085
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-01-16 15:15:37 +0900

    [Gold V] Title: 동전 1, Time: 132 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,23 +1,22 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
-
     	int n = Integer.parseInt(st.nextToken());
     	int k = Integer.parseInt(st.nextToken());
     	
-    	int[] arr = new int[n+1];
+    	int[] coins = new int[n];
     	int[] dp = new int[k+1];
     	dp[0] = 1;
     	
-    	for(int i = 1; i <= n; i++) {
-    		arr[i] = Integer.parseInt(br.readLine());
-    		for(int j = arr[i]; j <=k; j++) {
-    			dp[j] += dp[j-arr[i]];
+    	for(int i = 0; i < n; i++) {
+    		coins[i] = Integer.parseInt(br.readLine());
+    		for(int j = coins[i]; j <= k; j++) {
+    			dp[j] += dp[j-coins[i]];
     		}
     	}
     	System.out.println(dp[k]);
-    } 
-}
+    }
+} 
