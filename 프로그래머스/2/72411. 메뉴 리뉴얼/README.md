77bca336da8069104a96d5d9a539fe4f447bdafd
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-01-20 16:34:46 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 23.10 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
@@ -1,10 +1,10 @@
 # [level 2] 메뉴 리뉴얼 - 72411 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411?language=java) 
 
 ### 성능 요약
 
-메모리: 94.2 MB, 시간: 28.87 ms
+메모리: 84.4 MB, 시간: 23.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 16:33:8
+2024년 1월 6일 16:34:44
 
 ### 문제 설명
 
--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,7 +1,6 @@
 import java.util.*;
 class Solution {
     static HashMap<String, Integer> menu = new HashMap<>();
-    static int max;
     
     public String[] solution(String[] orders, int[] course) {
         List<String> answer = new ArrayList<>();
@@ -20,33 +19,19 @@ class Solution {
                 List<Integer> countList = new ArrayList<>(menu.values());
                 int max = Collections.max(countList);
 
-                if (max > 1)
-                    for (String key : menu.keySet())
-                        if (menu.get(key) == max)
-                            answer.add(key);
+                if (max > 1){
+                    for (String key : menu.keySet()){
+                        if (menu.get(key) == max) answer.add(key);
+                    }
+                }
                 menu.clear();
             }
         }
-//         for (int i = 0; i < course.length; i++) {
-//                 max = 0;
-// 				combin(s, "", course[j]);
-//                 max_Order[j] = max;
-// 			}
-//         for (String s : menu.keySet()){
-//             for (int i = 0; i < course.length; i++){
-//                 if (course[i] == s.length() && max_Order[i] > 1 && menu.get(s) == max_Order[i]){
-//                     answer.add(s);
-                    
-//                 }
-//             }
-//         }
-        
         return answer.stream().sorted().toArray(String[]::new);
     }
     private static void combin(String order, String s, int count){
         if(s.length() == count){
             menu.put(s, menu.getOrDefault(s, 0)+1);
-            max = Math.max(max, menu.get(s));
             return;
         }
         for(int i = 0; i < order.length(); i++){
