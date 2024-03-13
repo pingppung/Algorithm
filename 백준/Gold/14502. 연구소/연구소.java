7029095eefcaaea9855e457e306976bac1c4d5f2
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-14 00:46:40 +0900

    [Gold IV] Title: 연구소, Time: 836 ms, Memory: 297508 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,86 +1,73 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static class Point {
-		int x;
-		int y;
-		public Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-	static int m, n;
-	static int[][] arr;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	static int result = 0;
-	
-    public static void main(String[] args) throws IOException{
+	static int n, m, result = 0;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+    public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
     	n = Integer.parseInt(st.nextToken());
     	m = Integer.parseInt(st.nextToken());
-    	
-    	arr = new int[n][m];
-    	
+    	map = new int[n][m];
     	for(int i = 0; i < n; i++) {
-    		st = new StringTokenizer(br.readLine());
-    		for(int j = 0; j < m; j++) {
-    			arr[i][j] = Integer.parseInt(st.nextToken());
+    		String[] arr = br.readLine().split(" ");
+    		for(int j = 0; j < m ; j++) {
+    			map[i][j] = Integer.parseInt(arr[j]);
     		}
     	}
-    	dfs(0);
-    	
+    	buildWall(0);
     	System.out.println(result);
+    	
     }
-    public static void dfs(int depth) {
-    	if(depth == 3) {
-    		bfs();
+    private static void buildWall(int wall) {
+    	if(wall == 3) {
+    		virus();
     		return;
     	}
     	for(int i = 0; i < n; i++) {
-    		for(int j = 0; j < m; j++) {
-    			if(arr[i][j] == 0) {
-    				arr[i][j] = 1;
-    				dfs(depth +1);
-    				arr[i][j] = 0;
+    		for(int j = 0; j < m ; j++) {
+    			if(map[i][j] == 0) {
+    				map[i][j] = 1;
+    				buildWall(wall+1);
+    				map[i][j] = 0;
     			}
     		}
     	}
     }
-    public static void bfs() {
-    	Queue<Point> q = new LinkedList<>();
-    	int[][] newWall = new int[n][m];
+    private static void virus() {
+    	Queue<int[]> q = new LinkedList<>();
+    	int[][] newMap = new int[n][m];
     	for(int i = 0; i < n; i++) {
-    		for(int j = 0; j < m; j++) {
-    			newWall[i][j] = arr[i][j];
-    			if(newWall[i][j] == 2) q.offer(new Point(i, j));
+    		for(int j = 0; j < m ; j++) {
+    			newMap[i][j] = map[i][j];
+    			if(newMap[i][j] == 2) q.offer(new int[] {i, j});
     		}
     	}
     	while(!q.isEmpty()) {
-    		Point p = q.poll();
+    		int[] p = q.poll();
+    		
     		for(int i = 0; i < 4; i++) {
-        		int xx = p.x + dx[i];
-        		int yy = p.y + dy[i];
-        		if(xx >= 0 && yy >= 0 && xx < n && yy < m) {
-        			if(newWall[xx][yy] == 0) {
-        				q.offer(new Point(xx, yy));
-        				newWall[xx][yy] = 2;
-        			} 
-        		}
-        	}
-    	}
-    
-    	int count = 0;
-    	for(int i = 0; i < n; i++) {
-    		for(int j = 0; j < m; j++) {
-    			if(newWall[i][j] == 0) {
-    				count++;
+    			int xx = p[0] + dx[i];
+    			int yy = p[1] + dy[i];
+    			if(xx < 0 || yy < 0 || xx >= n || yy >= m) continue;
+    			if(newMap[xx][yy] == 0) {
+    				newMap[xx][yy] = 2;
+    				q.offer(new int[] {xx, yy});
     			}
     		}
     	}
-    	result = Math.max(result, count);
+    	result = countSafe(newMap);
     }
-    
-}
+    private static int countSafe(int[][] newMap) {
+    	int count = 0;
+    	for(int i = 0; i < n; i++) {
+    		for(int j = 0; j < m; j++) {
+    			if(newMap[i][j] == 0) count++;
+    		}
+    	}
+    	return Math.max(result, count);
+    }
+} 
