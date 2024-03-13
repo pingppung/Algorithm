be6d1c5ae11fe29f742864b63232a64e6d296770
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-14 01:29:36 +0900

    [Gold V] Title: 적록색약, Time: 152 ms, Memory: 16408 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,22 +1,22 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int n, X = 0, O = 0;
-	static char[][] arr;
+	static int n, count = 0, blindCount = 0;
 	static boolean[][] visited;
-    public static void main(String[] args) throws IOException{
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+    public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     	n = Integer.parseInt(br.readLine());
-    	
-    	arr = new char[n][n];
-    	
-    	
+    	char[][] map = new char[n][n];
+    	char[][] mapBlind = new char[n][n];
     	for(int i = 0; i < n; i++) {
     		String s = br.readLine();
     		for(int j = 0; j < n; j++) {
     			char color = s.charAt(j);
-    			arr[i][j] = color;
+    			map[i][j] = color;
+    			mapBlind[i][j] = color;
+    			if(color == 'G') mapBlind[i][j] = 'R';
     		}
     	}
 
@@ -24,43 +24,33 @@ public class Main {
     	for(int i = 0; i < n; i++) {
     		for(int j = 0; j < n; j++) {
     			if(!visited[i][j]) {
-    		    	X++;
-    		    	dfs(i,j);
+        	    	dfs(i, j, map);
+        	    	count++;
     			}
     		}
     	}
 
-    	for(int i = 0; i < n; i++) {
-    		for(int j = 0; j  < n; j++) {
-    			if(arr[i][j] == 'G') 
-    				arr[i][j] = 'R';
-    		}
-    	}
     	visited = new boolean[n][n];
     	for(int i = 0; i < n; i++) {
     		for(int j = 0; j < n; j++) {
     			if(!visited[i][j]) {
-    		    	O++;
-    		    	dfs(i,j);
+        	    	dfs(i, j, mapBlind);
+        	    	blindCount++;
     			}
     		}
     	}
-    	System.out.println(X + " "+ O);
-    	
-    } 	
-    public static void dfs(int x, int y) {
+    	System.out.println(count+ " "+blindCount);
+    }
+    private static void dfs(int x, int y, char[][] map) {
     	visited[x][y] = true;
-    	int []dx= {-1,1,0,0};
-    	int []dy= {0,0,-1,1};
-    	
+		char color = map[x][y];
     	for(int i = 0; i < 4; i++) {
-    		int xx = dx[i] + x;
-    		int yy = dy[i] + y;
-    		if (xx<0 || yy<0 || xx>=n || yy>=n) continue;
-    		if(!visited[xx][yy] && arr[x][y] == arr[xx][yy]) {
-    			dfs(xx,yy);
+    		int xx = x + dx[i];
+    		int yy = y + dy[i];
+    		if(xx < 0 || yy < 0 || xx >= n || yy >= n) continue;
+    		if(!visited[xx][yy] && color == map[xx][yy]) {
+    			dfs(xx, yy, map);
     		}
-    		
     	}
     }
-}
+} 
