c28ab6407d17bc899a7921fe2e40151abea24e57
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-13 15:17:38 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 512 ms, Memory: 116180 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,46 +1,38 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static StringBuilder sb = new StringBuilder();
+	static int n;
 	static int[][] graph;
 	static boolean[] visited;
-	static int n,m,count = 0;
-	public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
     	n = Integer.parseInt(st.nextToken());
-    	m = Integer.parseInt(st.nextToken());
-    	
-        graph = new int[n+1][n+1];
-        visited = new boolean[n+1];
-        for(int i = 0; i < m; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	int u = Integer.parseInt(st.nextToken());
-        	int v = Integer.parseInt(st.nextToken());
-        	graph[u][v] = graph[v][u] = 1;
-        }
-        for(int i = 1; i <= n; i++) {
-        	if(!visited[i]) {
-        		bfs(i);
-        		count++;
-        	}
+    	int m = Integer.parseInt(st.nextToken());
+    	graph = new int[n+1][n+1];
+    	visited = new boolean[n+1];
+    	for(int i= 0; i < m; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		int u = Integer.parseInt(st.nextToken());
+    		int v = Integer.parseInt(st.nextToken());
+    		graph[u][v] = graph[v][u] = 1;
+    	}
+    	int count = 0;
+    	for(int i = 1; i <= n; i++) {
+    		if(!visited[i]) {
+    			count++;
+    			dfs(i);
+    		}
     	}
     	System.out.println(count);
     }
-    static void bfs(int start) {
-    	Queue<Integer> q = new LinkedList<>();
-    	q.add(start);
-    	while(!q.isEmpty()) {
-    		start = q.poll();
-    		visited[start] = true;
-    		for(int i = 1; i <= n; i++) {
-    			if(graph[start][i] == 1 && !visited[i]) {
-        			q.add(i);
-        			visited[i] = true;
-        		}
+    private static void dfs(int num) {
+    	for(int i = 1; i <= n; i++) {
+    		if(!visited[i] && graph[num][i] == 1) {
+    			visited[i] = true;
+    			dfs(i);
     		}
+    		
     	}
-    
     }
-}
+} 
