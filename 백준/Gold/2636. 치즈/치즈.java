a4b68f822df4634116de1e8b4dc1621f22ce2168
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-21 23:24:15 +0900

    [Gold IV] Title: 치즈, Time: 156 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,18 +1,11 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int n, m, time = 0;
+	static int n, m, maxP = 0;
 	static int[][] map;
 	static boolean[][] visited;
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
-	static class Point{
-		int x, y;
-		Point(int x, int y){
-			this.x = x;
-			this.y = y;
-		}
-	}
     public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,42 +13,42 @@ public class Main {
     	m = Integer.parseInt(st.nextToken());
     	map = new int[n][m];
     	for(int i = 0; i < n; i++) {
-    		st = new StringTokenizer(br.readLine());
+    		String[] arr = br.readLine().split(" ");
     		for(int j = 0; j < m; j++) {
-    			map[i][j] = Integer.parseInt(st.nextToken());
+    			map[i][j] = Integer.parseInt(arr[j]);
     		}
     	}
-    	
-    	boolean chk = true;
-    	int beforeMelt = 0;
-    	while(chk) {
-    		time++;
+    	int time = 0;
+    	boolean zero = false;
+    	int cheese = 0;
+    	while(!zero) {
     		visited = new boolean[n][m];
-    		beforeMelt = getCheese();
-    		bfs();
-    		meltCheese();
+
+    		time++;
+    		cheese = getCheese();
+    		contactAir(); 
+    		melt();
     		int count = getCheese();
-    		if(count == 0) chk = false;
+    		if(count == 0) zero = true;
     	}
     	System.out.println(time);
-    	System.out.println(beforeMelt);
-    	
+    	System.out.println(cheese);
     }
-    private static void bfs() {
-    	Queue<Point> q = new LinkedList<>();
-    	q.add(new Point(0, 0));
+    private static void contactAir() {
+    	Queue<int[]> q = new LinkedList<>();
+    	q.add(new int[] {0, 0});
     	visited[0][0] = true;
     	while(!q.isEmpty()) {
-    		Point p = q.poll();
+    		int[] p = q.poll();
     		for(int i = 0; i < 4; i++) {
-        		int xx = p.x + dx[i];
-        		int yy = p.y + dy[i];
+        		int xx = p[0] + dx[i];
+        		int yy = p[1] + dy[i];
 
         		if(xx < 0 || yy < 0 || xx >= n || yy >= m) continue;
         		if(!visited[xx][yy]) {
     				visited[xx][yy] = true;
     				if(map[xx][yy] == 0) {
-        				q.add(new Point(xx, yy));
+        				q.add(new int[] {xx, yy});
         			}
     				else if(map[xx][yy] == 1) {
         				map[xx][yy] = 2;
@@ -65,7 +58,16 @@ public class Main {
         	}
     	}
     }
-    private static void meltCheese() {
+    private static int getCheese() {
+    	int count = 0;
+    	for(int i = 0; i < n; i++) {
+    		for(int j = 0; j < m; j++) {
+    			if(map[i][j] == 1) count++;
+    		}
+    	}
+    	return count;
+    }
+    private static void melt() {
     	for(int i = 0; i < n; i++) {
     		for(int j = 0; j < m; j++) {
     			if(map[i][j] == 2) {
@@ -74,15 +76,5 @@ public class Main {
     		}
     	}
     }
-    private static int getCheese() {
-    	int count = 0;
-    	for(int i = 0; i < n; i++) {
-    		for(int j = 0; j < m; j++) {
-    			if(map[i][j] == 1) {
-    				count++;
-    			}
-    		}
-    	}
-    	return count;
-    }
+   
 }  
