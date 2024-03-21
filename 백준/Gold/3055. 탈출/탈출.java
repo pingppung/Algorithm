ce3f30445d4f297b43ada031508455e5c7019fa5
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-21 17:54:50 +0900

    [Gold IV] Title: 탈출, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,8 +1,8 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int R, C, time = Integer.MAX_VALUE;
-	static char[][] map;
+	static int r, c, result = Integer.MAX_VALUE;
+	static char[][] forest;
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
 	static Queue<int[]> dochi = new LinkedList<>();
@@ -10,59 +10,61 @@ public class Main {
     public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
-    	R = Integer.parseInt(st.nextToken());
-    	C = Integer.parseInt(st.nextToken());
-    	map = new char[R][C];
-    	for(int i = 0; i < R; i++) {
-    		String s = br.readLine();
-    		for(int j = 0; j < C; j++) {
-        		map[i][j] = s.charAt(j);
-        		if(map[i][j] == 'S') {
-        			dochi.add(new int[] {i, j, 0});
-        		} else if(map[i][j] == '*') {
-        			water.add(new int[] {i, j});
-        		}
-        		
-        	}
+    	r = Integer.parseInt(st.nextToken());
+    	c = Integer.parseInt(st.nextToken());
+
+    	forest = new char[r][c];
+    	for(int i = 0; i < r; i++) {
+    		char[] arr = br.readLine().toCharArray();
+ 		   	for(int j = 0; j < c; j++) {
+ 			   forest[i][j] = arr[j];
+ 			   if(forest[i][j] == '*') water.offer(new int[] {i, j});
+ 			   if(forest[i][j] == 'S') dochi.offer(new int[] {i, j, 0});
+ 		   	}
+ 	   	}
+    	
+    	while(!dochi.isEmpty()) {
+    		flood();
+    		move();
     	}
-    	bfs();
-    	if(time == Integer.MAX_VALUE) System.out.println("KAKTUS");
-    	else System.out.println(time);
+    	if(result == Integer.MAX_VALUE) System.out.println("KAKTUS");
+    	else System.out.println(result);
     	
     }
-    private static void bfs() {
-    	while(!dochi.isEmpty()) {
-    		int wLen = water.size();
-    		for(int i = 0; i < wLen; i++) {
-    			int[] p = water.poll();
-        		for(int j = 0; j < 4; j++) {
-        			int xx = p[0] + dx[j];
-        			int yy = p[1] + dy[j];
-        			if(xx < 0 || yy < 0 || xx >= R || yy >= C) continue;
-        			if(map[xx][yy] == '.') {
-        				map[xx][yy] = '*';
-        				water.offer(new int[] {xx, yy});
-        			}
-        		}
+    private static void flood() {
+    	int wLen = water.size();
+		for(int i = 0; i < wLen; i++) {
+			int[] p = water.poll();
+    		for(int j = 0; j < 4; j++) {
+    			int xx = p[0] + dx[j];
+    			int yy = p[1] + dy[j];
+    			if(xx < 0 || yy < 0 || xx >= r || yy >= c) continue;
+    			if(forest[xx][yy] == '.') {
+    				forest[xx][yy] = '*';
+    				water.offer(new int[] {xx, yy});
+    			}
     		}
-    		int dLen = dochi.size();
-        	for(int i = 0; i < dLen; i++) {
-        		int[] p = dochi.poll();
-        		for(int j = 0; j < 4; j++) {
-        			int xx = p[0] + dx[j];
-        			int yy = p[1] + dy[j];
-        			int count = p[2];
-        			if(xx < 0 || yy < 0 || xx >= R || yy >= C) continue;
-        			if(map[xx][yy] == 'D') {
-        				time = Math.min(time, count+1);
-        				return;
-        			}
-        			if(map[xx][yy] == '.') {
-        				map[xx][yy] = 'S';
-        				dochi.offer(new int[] {xx, yy, count+1});
-        			}
-        		}
-        	}
+		}
+   }
+   
+    private static void move() {
+    	int dLen = dochi.size();
+    	for(int i = 0; i < dLen; i++) {
+    		int[] p = dochi.poll();
+    		for(int j = 0; j < 4; j++) {
+    			int xx = p[0] + dx[j];
+    			int yy = p[1] + dy[j];
+    			int count = p[2];
+    			if(xx < 0 || yy < 0 || xx >= r || yy >= c) continue;
+    			if(forest[xx][yy] == 'D') {
+    				result = Math.min(result, count+1);
+    				return;
+    			}
+    			if(forest[xx][yy] == '.') {
+    				forest[xx][yy] = 'S';
+    				dochi.offer(new int[] {xx, yy, count+1});
+    			}
+    		}
     	}
-    }
+   }
 }  
