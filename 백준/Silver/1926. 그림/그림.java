bc1c6c5652e3e7b022b6e6ec1c12a655ec7126a8
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-21 22:14:07 +0900

    [Silver I] Title: 그림, Time: 368 ms, Memory: 59092 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,56 +1,52 @@
 import java.io.*;
 import java.util.*;
 public class Main {
+	static int n, m, maxP = 0;
+	static int[][] paper;
 	static boolean[][] visited;
-	static int[][] arr;
-	static int n, m;
-	static int count, area = 0;
-    public static void main(String[] args) throws IOException{
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+    public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
     	n = Integer.parseInt(st.nextToken());
     	m = Integer.parseInt(st.nextToken());
-    	arr = new int[n][m];
+
+    	paper = new int[n][m];
     	visited = new boolean[n][m];
-    	
     	for(int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-    		for(int j = 0; j < m; j++) {
-    			arr[i][j] = Integer.parseInt(st.nextToken());
-    		}
-    	}
+    		String[] arr = br.readLine().split(" ");
+ 		   	for(int j = 0; j < m; j++) {
+ 			   paper[i][j] = Integer.parseInt(arr[j]);
+ 		   	}
+ 	   	}
     	
-    	int c = 0;
+    	int count = 0;
     	for(int i = 0; i < n; i++) {
-    		for(int j = 0; j < m; j++) {
-    			if(!visited[i][j] && arr[i][j] ==1) {
-    				c++;
-    				count = 0;
-    				dfs(i, j);
-    				area = Math.max(area, count);
-    			}
-    		}
-    	}
-    	System.out.println(c);
-    	System.out.println(area);
+ 		   	for(int j = 0; j < m; j++) {
+ 		   		if(!visited[i][j] && paper[i][j] == 1) {
+ 		   			int area = picture(i, j, 0);
+ 		   			count++;
+ 		   			maxP = Math.max(maxP, area);
+ 		   		}
+ 		   	}
+ 	   	}
+    	System.out.println(count);
+    	System.out.println(maxP);
     }
-	static void dfs(int x, int y){
-		visited[x][y] = true;
-		count++;
-		int[] dx = {-1, 1, 0, 0};
-		int[] dy = {0, 0, -1, 1};
-		for(int i = 0; i < 4; i++) {
-			int xx = x + dx[i];
-			int yy = y + dy[i];
-			
-			if(xx >= 0 && yy >= 0 && xx < n && yy < m) {
-				if(!visited[xx][yy] && arr[xx][yy] == 1) {
-					visited[xx][yy] = true;
-					dfs(xx, yy);
-				}
-			}
-		}
-	
-	}
-		
+    private static int picture(int x, int y, int size) {
+    	visited[x][y] = true;
+		int area = size + 1;
+    	for(int i = 0; i < 4; i++) {
+    		int xx = x + dx[i];
+    		int yy = y + dy[i];
+    		if(xx < 0 || yy < 0 || xx >= n || yy >= m) continue;
+    		if(!visited[xx][yy] && paper[xx][yy] == 1) {
+    			area = picture(xx, yy, area);
+    			visited[xx][yy] = true;
+    		}
+    	}
+    	return area;
+    }
+   
 }  
