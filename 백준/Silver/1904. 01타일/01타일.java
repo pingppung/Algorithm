5db4a1377d7dbfbcb9bc85653dfc962d3f0f28d4
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-23 01:51:31 +0900

    [Silver III] Title: 01타일, Time: 136 ms, Memory: 18136 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,19 +1,18 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int[] dp = new int[1000001];
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	int n = Integer.parseInt(br.readLine());
+    	int[] dp = new int[1000001];
     	dp[0] = 0;
     	dp[1] = 1;
     	dp[2] = 2;
-    	
     	for(int i = 3; i <= n; i++) {
-    		dp[i] = (dp[i-1] + dp[i-2])%15746;
+    		dp[i] = (dp[i-2] + dp[i-1])% 15746;
     	}
+    	
     	System.out.println(dp[n]);
-        
     }
     
-}  
+}
