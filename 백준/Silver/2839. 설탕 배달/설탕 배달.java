15d28b1ad8fe17b87af752ec4e3a732442ba8484
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-22 16:25:59 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,23 +1,23 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		int count = 0;
-		while(true) {
-			if(n % 5 == 0) {
-				count += n / 5;
-				break;
-			} 
-			if(n < 0) {
-				count = -1;
-				break;
-			}
-			n -= 3;
-			count++;
-		}
-		System.out.println(count);
-	}
-}
+	static int[] dp;
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int n = Integer.parseInt(br.readLine());
+    	int count = 0;
+    	while(true) {
+    		if(n%5==0) {
+    			count += n/5;
+    			break;
+    		}
+    		n -= 3; 
+    		count++;
+    		if(n < 0) {
+    			count = -1;
+    			break;
+    		}
+    	}
+    	System.out.println(count);
+    }
+}  
