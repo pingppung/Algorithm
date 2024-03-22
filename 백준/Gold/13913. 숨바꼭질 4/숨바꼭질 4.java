98c0177ef2f342ccd388b941a20a4a013c6f1c2d
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-22 13:11:06 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 392 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,51 +1,47 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int[] parent;
-	static int[] time;
+	static int[] map = new int[100001];
+	static int[] time = new int[100001];
+	
     public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
-    	int N = Integer.parseInt(st.nextToken());
-    	int K = Integer.parseInt(st.nextToken());
-    	parent = new int[100001];
-    	time = new int[100001];
-    	
-    	bfs(N, K);
-    	
+    	int n = Integer.parseInt(st.nextToken());
+    	int k = Integer.parseInt(st.nextToken());
+    	find(n, k);
     	Stack<Integer> stack = new Stack<>();
-        stack.push(K);
-        int index = K;
-        while (index != N) {
-            stack.push(parent[index]);
-            index = parent[index];
+        stack.push(k);
+        int index = k;
+        while (index != n) {
+            stack.push(map[index]);
+            index = map[index];
         }
         StringBuilder sb = new StringBuilder();
-        sb.append(time[K] - 1 + "\n");
+        sb.append(time[k] - 1 + "\n");
         while (!stack.isEmpty()) {
             sb.append(stack.pop() + " ");
         }
 
         System.out.println(sb.toString());
+    	
     }
-    private static void bfs(int N, int K) {
-    	Queue<Integer> q = new LinkedList<>();
-    	q.add(N);
-    	time[N] = 1;
+    private static void find(int n, int k) {
+    	Queue<Integer> q= new LinkedList<>();
+    	q.offer(n);
+    	time[n] = 1;
     	while(!q.isEmpty()) {
-    		int p = q.poll();
-    		if(p == K) return;
-    		int[] moves = {p + 1, p - 1, p * 2};
-    		for(int move : moves) {
+    		int num = q.poll();
+    		if(num == k) return;
+        	int[] moves = { num-1, num+1, 2*num};
+        	for(int move : moves) {
         		if(move < 0 || move > 100000) continue;
         		if(time[move] == 0) {
-        			q.add(move);
-        			time[move] = time[p] + 1;
-        			parent[move] = p;
+        			q.offer(move);
+        			time[move] = time[num] + 1;
+        			map[move] = num;
         		}
-         	}
+        	}
     	}
-    	
-    	
     }
 }  
