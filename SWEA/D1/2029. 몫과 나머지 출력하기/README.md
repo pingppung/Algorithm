f59ccca486154cc404e69a030622c729ff215fbb
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-05-17 00:23:42 +0900

    [D1] Title: 몫과 나머지 출력하기, Time: 114 ms, Memory: 26,628 KB -BaekjoonHub

---
+++ SWEA/D1/2029. 몫과 나머지 출력하기/README.md
@@ -0,0 +1,15 @@
+# [D1] 몫과 나머지 출력하기 - 2029 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QGNvKAtEDFAUq) 
+
+### 성능 요약
+
+메모리: 26,628 KB, 시간: 114 ms, 코드길이: 564 Bytes
+
+### 제출 일자
+
+2024-05-17 00:23
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2029. 몫과 나머지 출력하기/몫과 나머지 출력하기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		int idx = 1;
+		while(T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			System.out.printf("#%d %d %d%n", idx++, a/b, a%b);
+			
+		}
+	}
+
+}
