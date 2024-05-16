e25625e68d2890d6fdf95d783618630e7342c831
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-05-17 00:32:46 +0900

    [D1] Title: N줄덧셈, Time: 200 ms, Memory: 35,624 KB -BaekjoonHub

---
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int sum = IntStream.rangeClosed(1, N).sum();
+		System.out.println(sum);
+	}
+
+}
