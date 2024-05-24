9767bf722c805549e2d6ac4394dcc1e0b26a9930
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-05-24 21:33:56 +0900

    [Gold V] Title: LCS, Time: 216 ms, Memory: 28904 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,29 +1,22 @@
 import java.io.*;
 import java.util.*;
 public class Main {
+	static char[] s1, s2;
 	static Integer[][] dp;
-	static char[] s1;
-	static char[] s2;
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     	s1 = br.readLine().toCharArray();
     	s2 = br.readLine().toCharArray();
     	dp = new Integer[s1.length][s2.length];
-    	System.out.println(LCS(s1.length-1, s2.length-1));
+    	System.out.println(lcs(s1.length-1, s2.length-1));
     }
-    public static int LCS(int a, int b) {
-    	if(a<0||b<0) return 0;
-    	if(dp[a][b] ==null) {
+    private static int lcs(int a, int b) {
+    	if(a < 0 || b < 0 ) return 0;
+    	if(dp[a][b] == null) {
     		dp[a][b] = 0;
-    		if(s1[a] == s2[b])
-    			dp[a][b] = LCS(a-1,b-1)+1;
-    		else 
-    			dp[a][b] = Math.max(LCS(a-1,b), LCS(a, b-1));
-    		
+    		if(s1[a] == s2[b]) dp[a][b] = lcs(a-1, b-1)+1;
+    		else dp[a][b] = Math.max(lcs(a-1,b), lcs(a,b-1));
     	}
     	return dp[a][b];
     }
-
-} 
+}
