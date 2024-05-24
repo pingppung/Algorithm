31c14fd73de0b492f81dba5990e62c4530234041
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-05-25 02:35:56 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -1,14 +1,12 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st;
+    	int T = Integer.parseInt(br.readLine());
     	StringBuilder sb = new StringBuilder();
-    	
-    	int t = Integer.parseInt(br.readLine());
-    	while(t-- > 0) {
-    		st = new StringTokenizer(br.readLine());
+    	while(T-- > 0) {
+    		StringTokenizer st = new StringTokenizer(br.readLine());
     		int x = Integer.parseInt(st.nextToken());
     		int y = Integer.parseInt(st.nextToken());
     		int dis = y-x;
@@ -16,10 +14,7 @@ public class Main {
     		if(disSqrt == Math.sqrt(dis)) sb.append(disSqrt * 2 - 1).append("\n");
     		else if(disSqrt * disSqrt + disSqrt >= dis) sb.append(disSqrt * 2).append("\n");
     		else sb.append(disSqrt * 2 +1).append("\n");
-    		
     	}
-		System.out.println(sb);
-    	
+    	System.out.println(sb.toString());
     }
-    
 }
