11742465c95e22b762cf162da2b7b21cb131f915
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-05-24 13:41:48 +0900

    [Gold V] Title: 토마토, Time: 620 ms, Memory: 128160 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,75 +1,48 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static class Point {
-		int x;
-		int y;
-		int day;
-		public Point(int x, int y, int day) {
-			this.x = x;
-			this.y = y;
-			this.day = day;
-		}
-	}
-
-	static int m, n;
-	static int[][] arr;
-	static boolean[][] visited;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	static Queue<Point> q = new LinkedList<>();
-	static int result = 0;
-	
-    public static void main(String[] args) throws IOException{
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	static int m, n, result;
+	static Queue<int[]> q = new LinkedList<>();
+    public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
-
     	m = Integer.parseInt(st.nextToken());
     	n = Integer.parseInt(st.nextToken());
-    	
-    	arr = new int[n][m];
-    	visited = new boolean[n][m];
-    	
+    	int[][] box = new int[n][m];
     	for(int i = 0; i < n; i++) {
     		st = new StringTokenizer(br.readLine());
     		for(int j = 0; j < m; j++) {
-    			arr[i][j] = Integer.parseInt(st.nextToken());
-    			if(arr[i][j] == 1) q.offer(new Point(i, j, 0));
+    			box[i][j] = Integer.parseInt(st.nextToken());
+    			if(box[i][j] == 1) q.offer(new int[] {i, j, 0});
     		}
     	}
-    	bfs();
-    	
+    	bfs(box);
     	for(int i = 0; i < n; i++) {
     		for(int j = 0; j < m; j++) {
-    			if(arr[i][j] == 0) {
-    				result = -1;
-    				break;
+    			if(box[i][j] == 0) {
+    				System.out.println(-1);
+    				return;
     			}
     		}
     	}
-    	if(result != -1) System.out.println(result -1);
-    	else System.out.println(result);
+    	System.out.println(result);
     }
-    public static void bfs() {
+    private static void bfs(int[][] box) {
     	while(!q.isEmpty()) {
-    		Point p = q.poll();
-    		int period = p.day + 1;
+    		int[] now = q.poll();
+    		int date = now[2] + 1;
     		for(int i = 0; i < 4; i++) {
-        		int xx = p.x + dx[i];
-        		int yy = p.y + dy[i];
-        		if(xx >= 0 && yy >= 0 && xx < n && yy < m) {
-        			result = period;
-        			if(arr[xx][yy] == 0) {
-        				q.offer(new Point(xx, yy, period));
-        				arr[xx][yy] = 1;
-        			}
+    			int xx = now[0] + dx[i];
+        		int yy = now[1] + dy[i];
+        		if(xx < 0 || yy < 0 || xx >= n || yy >= m) continue;
+        		if(box[xx][yy] == 0) {
+        			box[xx][yy] = 1;
+        			q.offer(new int[] {xx, yy, date});
+        			result = date;
         		}
-        		
-        		
         	}
     	}
-    
-    	
     }
-	
 }
