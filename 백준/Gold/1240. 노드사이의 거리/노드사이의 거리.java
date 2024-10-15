435f162a14190b688297a12814c67622b4a32a44
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-10-15 13:31:39 +0900

    [Gold V] Title: 노드사이의 거리, Time: 236 ms, Memory: 17452 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -1,64 +1,57 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int n, m;
-	static int[][] tree;
-	static boolean[] visited;
-	static class Node {
-		int end, dis;
-		public Node(int end, int dis) {
+	private static class Node {
+		int end, dist;
+		public Node(int end, int dist) {
 			this.end = end;
-			this.dis = dis;
+			this.dist = dist;
 		}
 	}
-	static List<List<Node>> list = new ArrayList<>();
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
 
-		visited = new boolean[n+1];
-		for (int i = 0; i <= n; i++) {
-			list.add(new ArrayList<Node>());
-		}
-		for(int i = 0; i < n-1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int dis = Integer.parseInt(st.nextToken());
-			list.get(a).add(new Node(b, dis));
-			list.get(b).add(new Node(a, dis));
-		}
-		for(int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int d1 = Integer.parseInt(st.nextToken());
-			int d2 = Integer.parseInt(st.nextToken());
-			visited[d1] = true;
-			dfs(d1, d2, 0);
-			visited[d1] = false;
-		}
-		
-	}
-	public static void dfs(int start, int end, int dis) {
-		if(start == end) {
-			System.out.println(dis);
-			return;
-		}
-		for(int i = 0; i < list.get(start).size(); i++) {
-			int next = list.get(start).get(i).end;
-			int value = list.get(start).get(i).dis;
-			if(!visited[next]) {
-				visited[next] = true;
-				dfs(next, end, dis+value);
-				visited[next] = false;
-			}
-		}
-		
-	}
-	
+	private static boolean[] visited;
+	private static List<List<Node>> list = new ArrayList<>();
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int n = Integer.parseInt(st.nextToken());
+    	int m = Integer.parseInt(st.nextToken());
+    	
+    	for(int i = 0; i <= n; i++) {
+    		list.add(new ArrayList<>());
+    	}
+    	for(int i = 1; i < n; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		int a = Integer.parseInt(st.nextToken());
+    		int b = Integer.parseInt(st.nextToken());
+    		int dist = Integer.parseInt(st.nextToken());
+    		list.get(a).add(new Node(b, dist));
+    		list.get(b).add(new Node(a, dist));
+    	}
+    	visited = new boolean[n+1];
+    	for(int i = 1; i <= m; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		int e1 = Integer.parseInt(st.nextToken());
+    		int e2 = Integer.parseInt(st.nextToken());
+    		visited[e1] = true;
+    		calDist(e1, e2, 0);
+    		visited[e1] = false;
+    	}
+    }
+    private static void calDist(int cur, int end, int dist) {
+    	if(cur == end) {
+    		System.out.println(dist);
+    		return;
+    	}
+    	for(int i = 0; i < list.get(cur).size(); i++) {
+        	int next = list.get(cur).get(i).end;
+        	int val = list.get(cur).get(i).dist;
+        	if(!visited[next]) {
+        		visited[next] = true;
+        		calDist(next, end, dist+val);
+        		visited[next] = false;
+        	}
+        }
+    }
 }
-
 
