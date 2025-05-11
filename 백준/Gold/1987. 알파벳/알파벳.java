cde67b45376fe4478a838facb7ea8c3fabb56ce7
Pingppung <57535999+pingppung@users.noreply.github.com>
2025-05-11 16:29:31 +0900

    [Gold IV] Title: 알파벳, Time: 2168 ms, Memory: 302672 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,52 +1,50 @@
-import java.io.*;
 import java.util.*;
-public class Main {
-	static int R,C;
-	static int[][] arr;
-	static boolean[] visited;
-	static int result = 0;
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	StringBuilder sb = new StringBuilder();
-    	
-    	
-    	R = Integer.parseInt(st.nextToken());
-    	C = Integer.parseInt(st.nextToken());
-    	
-    	arr = new int[R][C];
-    	visited = new boolean[26];
-    	for(int i = 0; i < R; i++) {
-    		String s = br.readLine();
-    		for(int j = 0; j < C; j++) {
-    			char alph = s.charAt(j);
-    			arr[i][j] = alph -'A';
-    		}
-    	}
+import java.io.*;
 
-		dfs(0,0,0);
-    	System.out.println(result);
-    	
-    } 	
-    public static void dfs(int x, int y, int cnt) {
-    	if(visited[arr[x][y]]) {
-        	result = Math.max(result, cnt);
-    		return;
-    	}else {
-	    	visited[arr[x][y]] = true;
-	    	int[] dx = {-1, 1, 0, 0};
-	    	int[] dy = {0, 0, -1, 1};
-	    	
-	    	for(int i = 0; i < 4; i++) {
-	    		int xx = dx[i] + x;
-	    		int yy = dy[i] + y;
-	    		
-	    		if(xx >= 0 && yy >=0 && xx < R && yy <C) {
-	    			dfs(xx,yy ,cnt+1);
-	    			
-	    		}
-	    	}
-			visited[arr[x][y]] = false;
-    	}
-    }
+public class Main {
+	private static char[][] board;
+	private static int R, C, max_Dis = 0;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		board = new char[R+1][C+1];
+		for(int i = 1; i <= R; i++) {
+			String alph = br.readLine();
+			for(int j = 1; j <= C; j++) {
+				board[i][j] = alph.charAt(j-1);
+			}
+		}
+
+		Set<Character> path = new HashSet<>();
+		path.add(board[1][1]);
+		
+		movePiece(1, 1, path, 1);
+		System.out.println(max_Dis);
+		
+	}
+	private static void movePiece(int x, int y, Set<Character> path, int dis) {
+		max_Dis = Math.max(max_Dis, dis);
+		int[] dx = {-1,1,0,0};
+		int[] dy = {0,0,-1,1};
+		
+		for(int i = 0; i < 4; i++) {
+			int xx = dx[i] + x;
+			int yy = dy[i] + y;
+			
+			if(xx < 1 || yy < 1 || xx > R || yy > C) continue;
+			
+			char next = board[xx][yy];
+			if (!path.contains(next)) {
+	            path.add(next);
+	            movePiece(xx, yy, path, dis + 1);
+	            path.remove(next);
+	        }
+			
+		}
+	}
+	
+
 }
