0ab100e37a773ad2dda727489c7b74661179dccd
Pingppung <57535999+pingppung@users.noreply.github.com>
2025-05-13 14:47:09 +0900

    [Gold IV] Title: 공유기 설치, Time: 276 ms, Memory: 29268 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,36 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 public class Main {
-	static int[] houses;
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		houses = new int[n];
-		for(int i = 0; i < n; i++) {
+		
+		int N = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		int[] houses = new int[N];
+		for(int i = 0; i < N; i++) {
 			houses[i] = Integer.parseInt(br.readLine());
 		}
-		Arrays.sort(houses);
-		System.out.println(router(c, houses[n-1]));
-	}
 		
-	private static long router(int c, int max) {
-		int min_space = 1;
-		int max_space = max;
-		while(min_space <= max_space) {
+		Arrays.sort(houses);
+		
+		int min_dist = 1; 
+		int max_dist = houses[N-1] - houses[0];
+		while(min_dist <= max_dist) {
 			int count = 1;
 			int install_pos = houses[0];
-			int mid = (min_space + max_space) / 2;
+			int mid = (min_dist + max_dist) / 2;
+			
 			for(int house : houses) {
 				if(house - install_pos >= mid) {
 					count++;
 					install_pos = house;
 				}
 			}
-			if(count >= c) min_space = mid + 1;
-			else max_space = mid - 1;
+			if(count >= C) min_dist = mid + 1;
+			else max_dist = mid - 1;
 		}
-		return max_space;
+		
+		System.out.println(max_dist);
 	}
+	
 }
